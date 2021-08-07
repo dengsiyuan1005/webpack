1aa23a9d2cafa0384b8677a9d2860b320a6a0f1e
dengsiyuan <1057513859@qq.com>
2021-08-07 16:21:20 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
     output: {
-        filename: '[name].[contenthash].js'
-    }
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: '嘉慧',
+    })],
+
 };
+
+
+
+
